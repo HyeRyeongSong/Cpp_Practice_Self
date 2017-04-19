1d4ee118cb5164aa94d2457c32f4e47da3e9f851
= <=>
2017-04-19 10:23:47 +0900

    1장 inline 함수, namespce(전역변수, 지역변수)

--- PracticeCode/main.cpp
+++ PracticeCode/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+template <typename T>
+
+inline T test(T name)
+{
+    cout << name <<endl;
+}
+
+int iValue = 3;
+
+int main()
+{
+    int iValue = 5;
+    char* cName = new(char);
+    strcpy(cName, "hyeryung");
+    test(cName);
+    cout << "전역변수: " << ::iValue << endl << "지역변수: " << iValue << endl;
     return 0;
 }
