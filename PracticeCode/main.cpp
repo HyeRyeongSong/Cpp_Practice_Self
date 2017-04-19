26b812bac154c343ee66abe9c8c355ad74fb538c
= <=>
2017-04-19 21:02:21 +0900

    p.190 -> 문제 04-3(2) : 클래스

--- PracticeCode/main.cpp
+++ PracticeCode/main.cpp
@@ -1,33 +1,80 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
-struct Point
+namespace COMP_POS
 {
-    int xpos;
-    int ypos;
-};
+    enum
+    {
+        CLERK,
+        SENOIR,
+        ASSIST,
+        MANAGER
+    };
 
-Point& PntAdder(const Point &p1, const Point &p2)
-{
-    Point* ptr = new Point;
-    ptr->xpos = p1.xpos + p2.xpos;
-    ptr->ypos = p1.ypos + p2.ypos;
-    return *ptr;
+    void ShowPositionInfo(int iPosition)
+    {
+        switch(iPosition)
+        {
+            case CLERK:
+                cout << "사원" <<endl;
+                break;
+            case SENOIR:
+                cout << "주임" <<endl;
+                break;
+            case ASSIST:
+                cout << "대리" <<endl;
+                break;
+            case MANAGER:
+                cout << "과장" <<endl;
+                break;
+        }
+    }
+
 }
+
+class NameCard
+{
+private:
+    char* cName;
+    char* cCompanyName;
+    char* cPhoneNum;
+    int iPosition;
+
+public:
+    NameCard(char* cName, char* cCompanyName, char* cPhoneNum, int iPosition)
+            :iPosition(iPosition)
+    {
+        this->cName = new char[strlen(cName)+1];
+        this->cCompanyName = new char[strlen(cCompanyName)+1];
+        this->cPhoneNum = new char[strlen(cPhoneNum)+1];
+        strcpy(this->cName, cName);
+        strcpy(this->cCompanyName, cCompanyName);
+        strcpy(this->cPhoneNum, cPhoneNum);
+    }
+
+    void ShowNameCardInfo()
+    {
+        cout << "이름: " << this->cName <<endl;
+        cout << "회사: " << this->cCompanyName <<endl;
+        cout << "전화번호: " << this->cPhoneNum <<endl;
+        cout << "직급: ";
+        COMP_POS::ShowPositionInfo(this->iPosition);
+        cout << endl;
+    }
+
+    ~NameCard()
+    {
+        delete[] this->cName;
+        delete[] this->cCompanyName;
+        delete[] this->cPhoneNum;
+    }
+};
 
 int main()
 {
-    Point* ptr1 = new Point;
-    Point* ptr2 = new Point;
-    ptr1->xpos = 10;
-    ptr2->xpos = 5;
-    ptr1->ypos = 1;
-    ptr2->ypos = 2;
-    Point& ptr = PntAdder(*ptr1, *ptr2);
-    delete ptr1;
-    delete ptr2;
-    cout << "x: " << ptr.xpos << endl;
-    cout << "y: " << ptr.ypos << endl;
-    delete &ptr;
+    NameCard manClerk("Lee", "ABCENG", "010-1111-2222", COMP_POS::CLERK);
+    manClerk.ShowNameCardInfo();
+    return 0;
 }
