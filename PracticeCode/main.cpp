6c0de16755101ba6dc0cc0f920a96e6f2a16f994
= <=>
2017-04-20 00:48:08 +0900

    상속 생성자 소멸자

--- PracticeCode/main.cpp
+++ PracticeCode/main.cpp
@@ -3,95 +3,62 @@
 
 using namespace std;
 
-namespace COMP_POS
-{
-    enum
-    {
-        CLERK,
-        SENOIR,
-        ASSIST,
-        MANAGER
-    };
-
-    void ShowPositionInfo(int iPosition)
-    {
-        switch(iPosition)
-        {
-            case CLERK:
-                cout << "사원" <<endl;
-                break;
-            case SENOIR:
-                cout << "주임" <<endl;
-                break;
-            case ASSIST:
-                cout << "대리" <<endl;
-                break;
-            case MANAGER:
-                cout << "과장" <<endl;
-                break;
-        }
-    }
-
-}
-
-class NameCard
+class MyFriendInfo
 {
 private:
-    char* cName;
-    char* cCompanyName;
-    char* cPhoneNum;
-    int iPosition;
-
+    char * name;
+    int age;
 public:
-    NameCard(char* cName, char* cCompanyName, char* cPhoneNum, int iPosition)
-            :iPosition(iPosition)
+    MyFriendInfo(char* name, int age)
+    :age(age)
     {
-        this->cName = new char[strlen(cName)+1];
-        this->cCompanyName = new char[strlen(cCompanyName)+1];
-        this->cPhoneNum = new char[strlen(cPhoneNum)+1];
-        strcpy(this->cName, cName);
-        strcpy(this->cCompanyName, cCompanyName);
-        strcpy(this->cPhoneNum, cPhoneNum);
+        this ->name = new char[strlen(name)+1];
+        strcpy(this->name, name);
     }
-
-    /*explicit*/ NameCard(const NameCard& copy) //묵시적 형 변환 막아주는 키워드
-                                                //명시적 호출만 인정하겠다
-    :iPosition(copy.iPosition)
+    void ShowMyFriendInfo()
     {
-        this->cCompanyName = new char[strlen(copy.cCompanyName) + 1];
-        this ->cName = new char[strlen(copy.cName) + 1];
-        this->cPhoneNum = new char[strlen(copy.cPhoneNum) + 1];
-        strcpy(this->cName, copy.cName);
-        strcpy(this->cCompanyName, copy.cCompanyName);
-        strcpy(this->cPhoneNum,  copy.cPhoneNum);
+        cout << "이름: " <<name<<endl;
+        cout << "나이: " <<age<<endl;
     }
-
-    void ShowNameCardInfo()
+    ~MyFriendInfo()
     {
-        cout << "이름: " << this->cName <<endl;
-        cout << "회사: " << this->cCompanyName <<endl;
-        cout << "전화번호: " << this->cPhoneNum <<endl;
-        cout << "직급: ";
-        COMP_POS::ShowPositionInfo(this->iPosition);
-        cout << endl;
+        delete[] name;
     }
+};
 
-    ~NameCard()
+class MyFriendDetailInfo : public MyFriendInfo
+{
+private:
+    char* addr;
+    char* phone;
+public:
+    MyFriendDetailInfo(char* name, int age, char* addr, char* phone)
+    :MyFriendInfo(name, age)
     {
-        delete[] this->cName;
-        delete[] this->cCompanyName;
-        delete[] this->cPhoneNum;
+        this->addr = new char[strlen(addr)+1];
+        this->phone = new char[strlen(phone)+1];
+        strcpy(this->addr, addr);
+        strcpy(this->phone, phone);
+    }
+    void ShowMyFriendDetailInfo()
+    {
+        ShowMyFriendInfo();
+        cout << "주소: " <<addr<<endl;
+        cout << "전화: " <<phone<<endl;
+    }
+    ~MyFriendDetailInfo()
+    {
+        delete[] addr;
+        delete[] phone;
     }
 };
 
 int main()
 {
-    NameCard manClerk("Lee", "ABCENG", "010-1111-2222", COMP_POS::CLERK);
-    manClerk.ShowNameCardInfo();
-    NameCard man2(manClerk);
-    NameCard man3 = manClerk;
-    man2.ShowNameCardInfo();
-    const NameCard& tmp=NameCard("Lee", "ABCENG", "010-1111-2222", COMP_POS::CLERK);
-    NameCard("Lee", "ABCENG", "010-1111-2222", COMP_POS::CLERK).ShowNameCardInfo();
+    MyFriendDetailInfo MFD("hyeryung", 22, "hanti", "010-1111-1111");
+    MFD.ShowMyFriendDetailInfo();
+    cout << endl;
+    MFD.ShowMyFriendInfo();
+
     return 0;
 }
