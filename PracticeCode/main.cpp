6c0ddf223301d62f9661388ae589cffa7202def4
= <=>
2017-04-20 03:34:18 +0900

    p.439 문제 10-3 대체 왜 문제가 생기는 걸까...

--- PracticeCode/main.cpp
+++ PracticeCode/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <cstring>
 
 using namespace std;
 
@@ -7,53 +6,37 @@ class Point
 {
 private:
     int xpos, ypos;
+
 public:
     Point(int x=0, int y=0) : xpos(x), ypos(y)
     {}
-    void ShowPosition() const
-    {
-        cout<<'[' <<xpos<<", "<<ypos<<']'<<endl;
-    }
-    friend Point operator+(const Point &pos1, const Point &pos2);
-
-    Point& operator+=(const Point &pos)
-    {
-        this->xpos+=pos.xpos;
-        this->ypos+=pos.ypos;
-        return *this;
-    }
-
-    Point& operator-=(const Point &pos)
-    {
-        this->xpos-=pos.xpos;
-        this->ypos-=pos.ypos;
-        return *this;
-    }
+    friend ostream& operator<<(ostream&, const Point&);
+    friend istream& operator>>(istream&, Point&);
 };
 
-Point operator+(const Point &pos1, const Point &pos2)
+
+/*istream& operator>>(istream& is, Point pos)
 {
-    Point pos;
-    pos.xpos = pos1.xpos + pos2.xpos;
-    pos.ypos = pos1.ypos + pos2.ypos;
-    return pos;
+    is>>pos.xpos>>pos.ypos;
+    return is;
+}*/
+
+ostream& operator<< (ostream& os, Point pos)
+{
+    os<<'[' <<pos.xpos<<", "<<pos.ypos<<']'<<endl;
+    return os;
 }
 
-int main() {
-    Point pos1(1, 1);
-    Point pos2(2, 2);
-    Point pos3;
-    pos1.ShowPosition();
-    pos2.ShowPosition();
-    operator+(pos1, pos2);
-    pos1.operator+=(pos2);
-    pos1.ShowPosition();
-    pos2.ShowPosition();
-    pos2.operator-=(pos1);
-    pos1.ShowPosition();
-    pos2.ShowPosition();
+int main()
+{
+    Point pos1;
+    cout<<"x, y 좌표 순으로 입력: ";
+   // cin >> pos1;
+    cout<< pos1;
 
-
-
+    Point pos2;
+    cout<<"x, y 좌표 순으로 입력: ";
+    //cin >> pos2;
+    cout << pos2;
     return 0;
 }
