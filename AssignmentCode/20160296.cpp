350baf4c334087a9647c119cdf38c5a060ee9a45
= <=>
2017-06-15 11:43:58 +0900

    Assignment 3: ChainNode클래스를 관리하는 Chain 클래스 (SingleLinkedList 	/ ChainIterator)

--- AssignmentCode/20160296.cpp
+++ AssignmentCode/20160296.cpp
@@ -1,303 +1,240 @@
-//
-// Created by HyeRyeongSong on 2017. 4. 30..
-//
-
 #include <iostream>
-
-template <class T>
-class Bag
-{
-public:
-    Bag (int bagCapacity = 3);
-    Bag(const Bag<T>& copyBag);
-    Bag<T>& operator=(const Bag<T> ref);
-    ~Bag();
-
-    void ChangeSize1D(T*& a, const int oldSize, const int newSize);
-
-    int Size() const;
-    bool IsEmpty() const;
-    T& Element() const;
-
-    void Push(const T&);
-    void Pop();
-
-private:
-    T *array;
-    int capacity;
-    int top;
-};
 
 using namespace std;
 
-template <class T>
-Bag<T>::Bag (int bagCapacity): capacity ( bagCapacity )
-{
-    if (capacity < 1)
-        std::cout << "Capacity must be > 0" << std::endl;
-    array = new T[capacity];
-    top = -1;
-}
+template <class T> class Chain; // forward declaration
 
 template <class T>
-Bag<T>::Bag(const Bag<T>& copyBag):capacity(copyBag.capacity), top(copyBag.top)
+class ChainNode
 {
-    array = new T[capacity];
-    for(int i=0; i<Size(); ++i)
-    {
-        array[i] = copyBag.array[i];
-    }
-}
-
-template <class T>
-Bag<T>& Bag<T>::operator=(const Bag<T> ref)
-{
-    delete[] array;
-    array = new T[ref.capacity];
-    for(int i=0; i<ref.Size(); ++i)
-    {
-        array[i] = ref.array[i];
-    }
-    capacity = ref.capacity;
-    top = ref.top;
-    return *this;
-}
-
-
-
-template <class T>
-Bag<T>::~Bag()
-{
-    delete [ ] array;
-}
-
-template <class T>
-void Bag<T>::Push(const T& x)
-{
-    if (capacity ==top+1)
-    {
-        ChangeSize1D(array, capacity, 2 * capacity);
-        capacity *=2;
-    }
-    array[++top] = x;
-}
-
-template <class T>
-void Bag<T>::Pop()
-{
-    if (IsEmpty())
-        std::cout<< "Bag is empty, cannot delete"<<std::endl;
-    int deletePos = top / 2;
-    copy(array+deletePos+1,array+top+1,array+deletePos);
-    // compact array
-    array[top--].~T();   // destructor for T
-}
-
-template  <class T>
-inline int Bag<T>::Size() const
-{
-    return top+1;
-}
-
-template <class T>
-inline bool Bag<T>::IsEmpty() const
-{
-    return (Size() == 0);
-}
-
-template <class T>
-inline T& Bag<T>::Element() const
-{
-    if(IsEmpty())
-        std::cout<< "Bag is empty"<<std::endl;
-    return array[0];
-}
-
-template <class T>
-void Bag<T>::ChangeSize1D(T*& a, const int oldSize, const int newSize)
-{
-    if (newSize < 0)
-        std::cout<< "New length must be >= 0" << std::endl;
-
-    T* temp = new T[newSize];
-    int number = min(oldSize, newSize);
-    copy(a, a + number, temp);
-    delete [] a;
-    a = temp;
-}
-
-class Bizcard
-{
+    friend class Chain<T>;
 private:
-    char* cName;
-    char* cPhone;
-
+    T data;
+    ChainNode<T> *link;
 public:
-    Bizcard(char* cName, char* cPhone)
-    {
-        this->cName = new char[strlen(cName+1)];
-        strcpy(this->cName, cName);
-        this->cPhone = new char[strlen(cPhone+1)];
-        strcpy(this->cPhone, cPhone);
-    }
-
-    Bizcard(const Bizcard& bizcard)
-    {
-        cName = new char[strlen(bizcard.cName)];
-        strcpy(this->cName, bizcard.cName);
-        cPhone = new char[strlen(bizcard.cPhone)];
-        strcpy(this->cPhone, bizcard.cPhone);
-    }
-
-    Bizcard& operator=(const Bizcard ref)
-    {
-        delete[] this->cName;
-        delete[] this->cPhone;
-
-        cName = new char[strlen(ref.cName)];
-        strcpy(this->cName, ref.cName);
-        cPhone = new char[strlen(ref.cPhone)];
-        strcpy(this->cPhone, ref.cPhone);
-
-        return *this;
-    }
-
-
-    ~Bizcard()
-    {
-        delete[] cName;
-        delete[] cPhone;
-    }
-
-    void ShowInfo()
-    {
-        cout << "이름: " << this->cName << endl;
-        cout << "전화번호: " << this->cPhone << endl;
-    }
+    ChainNode(T element = 0, ChainNode* next = 0)
+    // 0 is the default value for element and next
+    {data=element; link=next;}
 };
 
-void CheckResult(Bag<int> bag);
-void CheckResult(Bag<Bizcard> bag);
+template <class T>
+class Chain {
+public:
+    Chain()
+    {
+        first = 0;
+        last = 0;
+    }; // constructor initializing first to 0
+
+    ~Chain()
+    {
+        for(ChainNode<T>* chainNode = first; chainNode != last; )
+        {
+            ChainNode<T>* deletedNode = chainNode;
+            chainNode = chainNode->link;
+            delete deletedNode;
+        }
+        delete last;
+    }
+
+
+    T Front()
+    {
+        return first->data;
+    }
+
+    T Back()
+    {
+        return last->data;
+    }
+
+    ChainNode<T>* Get(int iIndex)
+    {
+        ChainNode<T>* getNode = first;
+        for(int i = 0; i<iIndex; ++i)
+        {
+            if(getNode->link == 0)
+            {
+                cout<< "존재하지 않는 인덱스입니다" <<endl;
+                return 0;
+            }
+            getNode = getNode->link;
+        }
+        return getNode;
+    }
+
+    T GetData(int iIndex)
+    {
+        ChainNode<T>* getNode = first;
+        for(int i = 0; i<iIndex; ++i)
+        {
+            if(getNode->link == 0)
+            {
+                cout<< "존재하지 않는 인덱스입니다" <<endl;
+                return 0;
+            }
+            getNode = getNode->link;
+        }
+        return getNode->data;
+    }
+
+
+    void InsertFront(T element){
+        if(first)
+        {
+            ChainNode<T> *newNode = new ChainNode<T>(element);
+            newNode->link = first;
+            first = newNode;
+        }
+        else first = last = new ChainNode<T>(element);
+    }
+
+    void InsertBack(const T& e)
+    {
+        if (first) {// nonempty chain
+            last->link = new ChainNode<T>(e);
+            last = last->link;
+        }
+        else first = last = new ChainNode<T>(e);
+    }
+
+    void InsertbyIndex(const T& e, int iIndex)
+    {
+        if(first)
+        {
+            ChainNode<T>* newNode = new ChainNode<T>(e, Get(iIndex));
+            ChainNode<T>* preNode = Get(iIndex-1);
+            preNode->link = newNode;
+        }
+        else InsertFront(e);
+    }
+
+    void Delete(ChainNode<T> *x, ChainNode<T> *y=0)
+    {
+        if (x == first) first = first->link;
+        else if(x == last)
+        {
+            last = y;
+            y->link = x->link;
+        }
+        else y->link = x->link;
+        delete x;
+    }
+
+    void DeleteFront()
+    {
+        Delete(first);
+    }
+
+    void DeleteBack()
+    {
+        ChainNode<T>* pre;
+        for(ChainNode<T>* searchNode = first; searchNode !=last; )
+        {
+            pre = searchNode;
+            searchNode = searchNode->link;
+        }
+        Delete(last, pre);
+    }
+
+    void DeletebyIndex(int iIndex)
+    {
+        Delete(Get(iIndex), Get(iIndex-1));
+    }
+
+
+    void PrintAllNode()
+    {
+        ChainIterator * chainIterator = new ChainIterator();
+        for(*chainIterator = begin(); *chainIterator != end(); ++chainIterator)
+        {
+            cout << chainIterator->operator->() << "  ";
+        }
+
+        cout <<endl;
+    }
+
+    ChainIterator begin() {return ChainIterator(first);}
+    ChainIterator end() {return ChainIterator(0);}
+
+    class ChainIterator {
+    public:
+        // typedefs required by C++ for a forward iterator omitted
+
+        // constructor
+        ChainIterator(ChainNode<T>* startNode = 0)
+        {current = startNode;}
+
+        // dereferencing operators
+        T& operator*() const {return current->data;}
+        T* operator->() const {return &current->data;}
+
+        // increment
+        ChainIterator& operator++()   // preincrement
+        {current = current->link; return *this;}
+        ChainIterator operator++(int) // postincrement
+        {
+            ChainIterator old = *this;
+            current = current->link;
+            return old;
+        }
+
+        // equality testing
+        bool operator!=(const ChainIterator right) const
+        {return current != right.current;}
+        bool operator==(const ChainIterator right) const
+        {return current == right.current;}
+        
+    private:
+        ChainNode<T>* current;
+    };
+
+
+private:
+    ChainNode<T> *first;
+    ChainNode<T> *last;
+};
 
 int main()
 {
-    Bag<int> bag;
-//    Bag<Bizcard> bag1;
+    Chain<int> intlist;
 
-    cout << "빈 Bag에 대한 결과" <<endl;
-    CheckResult(bag);
-
-    for(int i=0; i<3; ++i)
+    for(int i=0; i<9; ++i)
     {
-        int iNum = 0;
-        cout << "숫자를 하나 입력하세요: " <<endl;
-        cin >>iNum;
-        bag.Push(iNum);
+        intlist.InsertBack(i+1);
     }
 
-    cout << "정수 3개 추가 후 결과" <<endl;
-    CheckResult(bag);
+    intlist.PrintAllNode();
 
-    for(int i=0; i<4; ++i)
-    {
-        int iNum = 0;
-        cout << "숫자를 입력하세요: " <<endl;
-        cin >>iNum;
-        bag.Push(iNum);
-    }
+    intlist.InsertFront(0);
 
-    cout << "정수 4개 추가 후 결과" <<endl;
-    CheckResult(bag);
+    intlist.PrintAllNode();
 
-    for(int i=0; i<2; ++i)
-    {
-        bag.Pop();
-    }
+    intlist.InsertBack((10));
 
-    cout << "정수 2개 제 후 결과" <<endl;
-    CheckResult(bag);
-/*
-    cout << endl << endl <<"--------------------------------------------------" <<endl << endl;
-    cout << "빈 Bag에 대한 결과" <<endl;
-    CheckResult(bag1);
+    intlist.PrintAllNode();
 
-    char* cName = new char[256];
-    char* cPhone = new char[256];
+    cout << intlist.Front() << endl;
 
-    for(int i=0; i<3; ++i)
-    {
-        cout << "이름을 입력하세요: ";
-        cin >>cName;
-        cout << "전화번호를 입력하세요: ";
-        cin >>cPhone;
-        Bizcard bizcard(cName, cPhone);
-        bag1.Push(bizcard);
-    }
+    cout << intlist.Back() << endl;
 
-    cout << "객체 3개 추가 후 결과" <<endl;
-    CheckResult(bag1);
+    cout << intlist.GetData(2) << endl;
 
-    for(int i=0; i<4; ++i)
-    {
-        cout << "이름을 입력하세요: ";
-        cin >>cName;
-        cout << "전화번호를 입력하세요: ";
-        cin >>cPhone;
-        Bizcard bizcard(cName, cPhone);
-        bag1.Push(bizcard);
-    }
+    intlist.DeleteFront();
 
-    cout << "객체 4개 추가 후 결과" <<endl;
-    CheckResult(bag1);
+    intlist.PrintAllNode();
 
-    for(int i=0; i<2; ++i)
-    {
-        bag1.Pop();
-    }
+    intlist.DeleteBack();
 
-    cout << "객체 2개 제 후 결과" <<endl;
-    CheckResult(bag1);
+    intlist.PrintAllNode();
 
-    delete[] cName;
-    delete[] cPhone;
-*/
+    intlist.InsertbyIndex(100, 3);
+
+    intlist.PrintAllNode();
+
+    intlist.DeletebyIndex(6);
+
+    intlist.PrintAllNode();
 
     return 0;
-
 }
-
-void CheckResult(Bag<int> bag)
-{
-    cout << "bag.Size(): " << bag.Size() << endl;
-    cout << "bag.IsEmpty(): ";
-    if(bag.IsEmpty())
-    {
-        cout<< "true" <<endl;
-    }
-    else
-    {
-        cout << "false" <<endl;
-    }
-    cout << "bag.Element(): " << bag.Element() << endl;
-    cout << endl;
-}
-
-void CheckResult(Bag<Bizcard> bag1)
-{
-    cout << "bag.Size(): " << bag1.Size() << endl;
-    cout << "bag.IsEmpty(): ";
-    if(bag1.IsEmpty())
-    {
-        cout<< "true" <<endl;
-    }
-    else
-    {
-        cout << "false" <<endl;
-    }
-    cout << "bag.Element(): ";
-    Bizcard element = bag1.Element();
-    element.ShowInfo();
-    cout << endl;
-}
-
-
---
+++ AssignmentCode/cmake-build-debug/CMakeFiles/20160296.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/20160296.dir/20160296.cpp.o"
+  "CMakeFiles/20160296.dir/just.cpp.o"
+  "20160296.pdb"
+  "20160296"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/20160296.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
